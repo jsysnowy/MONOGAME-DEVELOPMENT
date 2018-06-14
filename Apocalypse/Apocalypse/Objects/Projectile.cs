00160f72753c82725b662345688ec2fc887b9c75
Samantha <axispeace@hotmail.co.uk>
2018-06-14 20:15:13 +0100

    fsdersgdfrx

--- Apocalypse/Apocalypse/Objects/Projectile.cs
+++ Apocalypse/Apocalypse/Objects/Projectile.cs
@@ -8,7 +8,7 @@ using Microsoft.Xna.Framework;
 namespace Apocalypse.Objects {
     class Projectile : CoreEngine.GameObject {
         // Projectile speed:
-        float PROJECTILESPEED = 99;
+        float PROJECTILESPEED = 80;
 
 
         // Target:
